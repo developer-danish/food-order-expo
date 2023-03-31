5a71e33ac4e04a4a42856187520e41ca89bd50b1
Danish <danishnazir2001@gmail.com>
2023-04-01 01:55:58 +0530

    payment and orders api hit

--- pages/Orders.js
+++ pages/Orders.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 import {
   Image,
   ScrollView,
@@ -6,27 +7,29 @@ import {
   Text,
   useColorScheme,
   View,
-} from 'react-native';
+} from "react-native";
 // import type { Node } from 'react';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { useEffect } from "react";
+import { apiObject } from "./../api/auth";
 
 const styles = StyleSheet.create({
   ordersRow: {
     flexDirection: "row",
   },
   headerRowItem: {
-    flex: .25,
-    padding: 8
+    flex: 0.25,
+    padding: 8,
   },
   headerTitle: {
     fontWeight: "700",
-    color: "#000"
+    color: "#000",
   },
   orderImage: {
     height: 50,
     width: 80,
-    borderRadius: 4
-  }
+    borderRadius: 4,
+  },
 });
 
 const OrderRow = ({ order }) => {
@@ -36,34 +39,46 @@ const OrderRow = ({ order }) => {
         <Image style={styles.orderImage} source={order.image} />
       </View>
       <View style={styles.headerRowItem}>
-        <Text >{order.title}</Text>
+        <Text>{order.title}</Text>
       </View>
       <View style={styles.headerRowItem}>
-        <Text >$ {order.price}</Text>
+        <Text>$ {order.price}</Text>
       </View>
       <View style={styles.headerRowItem}>
-        <Text >{order.status}</Text>
+        <Text>{order.status}</Text>
       </View>
     </View>
   );
-}
-const orders = [
-  {
-    title: "Product name 1",
-    image: require("../assets/foodnew.jpeg"),
-    status: "Pending",
-    price: "3"
-  },
-  {
-    title: "Product name 2",
-    image: require("../assets/foodnew.jpeg"),
-    status: "Pending",
-    price: "3"
-  }
-]
+};
+// const orders = [
+//   {
+//     title: "Product name 1",
+//     image: require("../assets/foodnew.jpeg"),
+//     status: "Pending",
+//     price: "3",
+//   },
+//   {
+//     title: "Product name 2",
+//     image: require("../assets/foodnew.jpeg"),
+//     status: "Pending",
+//     price: "3",
+//   },
+// ];
 
 export const OrdersPage = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [orders, setOrders] = useState([]);
+  const user = { _id: "63333e15df7678d1e1224fad" };
+  useEffect(() => {
+    apiObject
+      .post("api/order/userspecificorders", user)
+      .then(async (response) => {
+        setOrders(response.data.orders);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -71,7 +86,8 @@ export const OrdersPage = () => {
   return (
     <ScrollView
       contentInsetAdjustmentBehavior="automatic"
-      style={backgroundStyle}>
+      style={backgroundStyle}
+    >
       <View style={styles.ordersRow}>
         <View style={styles.headerRowItem}>
           <Text style={styles.headerTitle}>Image</Text>
@@ -86,7 +102,9 @@ export const OrdersPage = () => {
           <Text style={styles.headerTitle}>Status</Text>
         </View>
       </View>
-      {orders.map((order, index) => <OrderRow key={index} order={order} />)}
+      {orders.map((order, index) => (
+        <OrderRow key={index} order={order} />
+      ))}
     </ScrollView>
   );
 };
