7c9cc21bae24d18e646ff9e57319a23be905d108
Mac <mac@Reyazs-MBP.hgu_lan>
2023-05-08 12:46:22 +0530

    changed backend url

--- helpers/Constants.js
+++ helpers/Constants.js
@@ -1,2 +1,7 @@
-export const BaseUrl = "https://food-order-b6n5.onrender.com/";
-export const BaseImageUrl = "https://food-order-b6n5.onrender.com/uploads/";
+// export const BaseUrl = "https://food-order-b6n5.onrender.com/";
+// export const BaseImageUrl = "https://food-order-b6n5.onrender.com/uploads/";
+
+const ipAddress = "192.168.1.39"; //Enter your current ip address using cmd ipconfig
+
+export const BaseUrl = `http://${ipAddress}:3500/`;
+export const BaseImageUrl = `http://${ipAddress}:3500/uploads/`;
